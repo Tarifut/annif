211e28dc1c392859e3fcc764397b7fd79d7f20e2
JCR <tarifut@gmail.com>
2024-11-13 20:05:09 +0100

    four

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -66,28 +66,34 @@ export default function Home() {
         <source src="music.mp3" type="audio/mpeg" />
         
       </audio>
-      <h1 className='m-4 text-8xl text-center'>Anniversaire Adeline et Vincent</h1>
+      <h1 className='m-4 2xl:text-6xl lg:text-4xl sm:text-2xl text-center'>Anniversaire Adeline et Vincent</h1>
 
       <Confetti />
 
       <div className="flex items-center flex-wrap gap-4 justify-center ">
-        <Image src={pola}
+        <p className="w-1/4 h-1/4" ><Image src={pola}
+        layout="responsive"
           width={450}
           height={450}
-          alt="Polaroid"></Image>
+          
+          alt="Polaroid"></Image></p>
 
-<Image src={texte}
+<p className="w-1/4 h-1/4" ><Image src={texte}
+layout="responsive"
           width={450}
           height={450}
-          alt="Polaroid"></Image>
+          
+          alt="Polaroid"></Image></p>
 
-        <Image src={cochon}
+<p className="w-1/4 h-1/4" ><Image src={cochon}
+        layout="responsive"
           width={450}
           height={450}
-          alt="Polaroid"></Image>
+          
+          alt="Polaroid"></Image></p>
 
 {isLoaded && <GoogleMap
-      mapContainerStyle={containerStyle}
+      mapContainerStyle={{width:'50vw',height:'25vh'}}
       center={center}
       zoom={zoom}
       onLoad={onLoad}
