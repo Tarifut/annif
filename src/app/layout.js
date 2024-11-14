e95f9e9d7ac204d06fbf66acf4b013e737788c2c
JCR <tarifut@gmail.com>
2024-11-14 12:18:12 +0100

    compressed-2

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,21 +41,22 @@ export default function RootLayout({ children }) {
 
       <body className={`p-4 w-screen	h-screen bg-black text-white ${breathing.className}  antialiased overflow-x-hidden	`}>
 
-        {!showNav && <button onClick={handleEnter} className=" m-4 2xl:text-9xl lg:text-7xl sm:text-5xl text-center w-screen h-screen flex items-center justify-center"><p className=" transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300">Entrer</p></button>}
+        {!showNav && <button onClick={handleEnter} className="  2xl:text-9xl lg:text-7xl sm:text-5xl text-center w-screen h-screen flex items-center justify-center"><p className=" transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300">Entrer</p></button>}
 
 {showNav && <audio autoPlay loop>
         <source src="/music.mp3" type="audio/mpeg" />
       </audio>}
       
-        {showNav && <nav>
+        {showNav && <section> <Confetti /><nav className="mb-24">
           
-          <Confetti />
-          <ol className=" mb-24 p-4 flex flex-row gap-44 justify-center 2xl:text-2xl lg:text-xl sm:text-lg">
-            <Link href="/accueil"  className= "transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300"><p>Accueil</p></Link>
+         
+          <ol className=" m-auto  p-4 flex w-3/4 flex-row   justify-between 2xl:text-2xl lg:text-xl sm:text-lg">
+            <Link href="/accueil"  className= " transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300"><p>Accueil</p></Link>
             <Link href="/programme"  className= "transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300" ><p>Programme</p></Link>
             <Link href="/informations" className= "transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300" ><p>Informations</p></Link>
           </ol>
         </nav>
+        </section>
 
 
         }
