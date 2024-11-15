c0186fccbc0ec49d34f7a4e9638d7095c43ef5fd
JCR <tarifut@gmail.com>
2024-11-15 10:07:11 +0100

    V1.4

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,9 +38,9 @@ export default function RootLayout({ children }) {
     }
 
   return (
-    <html className="h-full	">
+    <html className="h-dvh 	">
 
-      <body className={` w-full	h-full bg-black text-white ${breathing.className}  antialiased overflow-x-hidden	`}>
+      <body className={` w-dvh 	h-dvh  bg-black text-white ${breathing.className}  antialiased overflow-x-hidden	`}>
 
         {!showNav && <button onClick={handleEnter} className="  2xl:text-9xl lg:text-7xl sm:text-5xl text-center w-screen h-screen flex items-center justify-center"><p className=" transition ease-in-out 0 hover:-translate-y-1 hover:scale-150 duration-300">Entrer</p></button>}
 
